c2234af104f4f7fa6c8d8b60001d7d6633d885a8
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-08-26 13:24:01 -0700

    add: free and paid app flavours

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/LoadCustomIntentTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/LoadCustomIntentTest.java
@@ -21,7 +21,6 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.example.android.jokeandroidlib.AndroidLibConstants.JOKE;
-import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
 public class LoadCustomIntentTest {
--- app/src/androidTest/java/com/udacity/gradle/builditbigger/LoadJokeTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/LoadJokeTest.java
@@ -1,15 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
-import android.content.ComponentName;
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
-import com.example.android.jokeandroidlib.JokeActivity;
-
-import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,11 +12,8 @@ import java.util.concurrent.TimeUnit;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.example.android.jokeandroidlib.AndroidLibConstants.JOKE;
 import static org.hamcrest.Matchers.not;
 
 @RunWith(AndroidJUnit4.class)
