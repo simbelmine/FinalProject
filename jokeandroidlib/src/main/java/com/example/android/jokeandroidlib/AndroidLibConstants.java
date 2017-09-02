db18fbd9f0b817e72c69be4610cd14e77668093f
Svetoslava Delcheva <simbelmine.sve@gmail.com>
2017-09-02 15:10:05 -0700

    update: Make constant class (AndroidLibConstants) final

--- jokeandroidlib/src/main/java/com/example/android/jokeandroidlib/AndroidLibConstants.java
+++ jokeandroidlib/src/main/java/com/example/android/jokeandroidlib/AndroidLibConstants.java
@@ -1,9 +1,8 @@
 package com.example.android.jokeandroidlib;
 
-/**
- * Created by Sve on 8/20/17.
- */
 
-public class AndroidLibConstants {
+public final class AndroidLibConstants {
+    private AndroidLibConstants(){}
+
     public static final String JOKE = "joke_content";
 }
